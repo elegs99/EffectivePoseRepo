ed884cc9aa523785ffedfb48120ec7b4e87c8f5c
Ethan Legum <71243839+elegs99@users.noreply.github.com>
2024-04-03 18:32:55 -0400

    Simplified Camera Implementation

--- EffectivePose/app/src/main/java/com/example/effectivepose/MainActivity.java
+++ EffectivePose/app/src/main/java/com/example/effectivepose/MainActivity.java
@@ -1,67 +1,63 @@
 package com.example.effectivepose;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
-import androidx.camera.view.PreviewView;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
-
-import com.google.common.util.concurrent.ListenableFuture;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.media.Image;
 import android.os.Bundle;
-import android.view.SurfaceView;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CAMERA_PERMISSION = 200;
-    private CameraCaptureSurface rgbCapture;
-    private SurfaceView imageView;
+    private ProcessCameraProvider cameraProvider;
+    private PreviewView previewWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
+        previewWindow = findViewById(R.id.previewWindow);
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
         } else {
-            initializeCameraSurfaces();
-        }
-    }
-    private void initializeCameraSurfaces() {
-        rgbCapture = new CameraCaptureSurface(getApplicationContext(), imageView);
+            ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
+            cameraProviderListenableFuture.addListener(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        cameraProvider = cameraProviderListenableFuture.get();
 
-        rgbCapture.open("0");
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CAMERA_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initializeCameraSurfaces();
-            } else {
-                // Handle case where camera permission is denied.
-            }
+                        StartCameraX(cameraProvider);
+
+                    } catch (ExecutionException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }, ContextCompat.getMainExecutor(this));
         }
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (rgbCapture != null) {
-            rgbCapture.close();
+
+    private void StartCameraX(ProcessCameraProvider cameraProvider) {
+        CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewWindow.getSurfaceProvider());
+        try {
+            cameraProvider.unbindAll();
+
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
